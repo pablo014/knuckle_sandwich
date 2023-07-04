cbb52162bee5aee9d8cd99b63e90ec2f6635f26d
pablo014 <angelopablo014@gmail.com>
2023-07-04 02:50:23 -0700

    feat(carousel): add image banner

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
-import VueDisplacementSlideshow from "vue-displacement-slideshow";
-const images: Array<string> = [];
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
+const images = [
+    './src/assets/sandwich/blatt.webp',
+    './src/assets/sandwich/fl_veggie.webp',
+    './src/assets/sandwich/pastrami.webp',
+];
 </script>
 
 <template>
   <div>
       <div id="banner">
-          <VueDisplacementSlideshow
-                  :images="images"
-                  ref="slideshow"
-
-          />
+          <vueper-slides autoplay :touchable="false" :arrows="false" :bullets="false">
+              <vueper-slide
+                      v-for="(slide, i) in images"
+                      :key="i"
+                      >
+                  <template #content>
+                      <img class="w-full h-full" :src="slide" />
+                  </template>
+              </vueper-slide>
+          </vueper-slides>
       </div>
   </div>
 </template>
