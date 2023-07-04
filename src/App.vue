ddc6fe159f715ea7d60b489c1b2b0ff19b0ddeb4
pablo014 <angelopablo014@gmail.com>
2023-07-04 01:42:39 -0700

    feat(assets): add logo and breakfast

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <script setup lang="ts">
+import VueDisplacementSlideshow from "vue-displacement-slideshow";
+const images: Array<string> = [];
 </script>
 
 <template>
   <div>
+      <div id="banner">
+          <VueDisplacementSlideshow
+                  :images="images"
+                  ref="slideshow"
 
+          />
+      </div>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
